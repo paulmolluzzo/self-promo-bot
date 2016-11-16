7e5e4bb73759c72655390978d5760d0ba11113bc
Paul Molluzzo <paul@molluzzo.com>
2016-11-15 23:05:55 -0500

    Replace emoji with 983$ for Isaora

--- bot-methods.js
+++ bot-methods.js
@@ -251,7 +251,7 @@ const botMethods = {
 			buttons: [{
 				type: 'web_url',
 				url: `https://www.isaora.com/`,
-				title: 'Spend that 💰'
+				title: 'Spend that $$$'
 			}, {
 				type: 'postback',
 				title: 'View Tech Stack',
